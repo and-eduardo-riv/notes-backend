26637a13365ba3cff9d6acb332cfc04316f9919d
Eduardo Rivero <eduardo.rivero@unosquare.com>
2023-11-20 18:07:13 -0400

    refactor(index): move method to generateId & validate content in json body for post

--- index.js
+++ index.js
@@ -21,6 +21,11 @@ let notes = [
   },
 ];
 
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
@@ -48,10 +53,19 @@ app.delete("/api/notes/:id", (request, response) => {
 });
 
 app.post("/api/notes", (request, response) => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  const note = request.body;
-  note.id = maxId + 1;
+  const body = request.body;
 
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    id: generateId(),
+    content: body.content,
+    important: body.important || false,
+  };
   notes = notes.concat(note);
 
   response.json(note);
