960c72965a703c3a58af0ec024fc769e33b95825
Eduardo Rivero <eduardo.rivero@unosquare.com>
2023-11-21 16:17:27 -0400

    feat(index): change enpdoints to post & get a note

--- index.js
+++ index.js
@@ -49,14 +49,9 @@ app.get("/api/notes", (request, response) => {
 });
 
 app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-
-  if (note) {
+  Note.findById(request.params.id).then((note) => {
     response.json(note);
-  } else {
-    response.status(404).end();
-  }
+  });
 });
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -69,20 +64,20 @@ app.delete("/api/notes/:id", (request, response) => {
 app.post("/api/notes", (request, response) => {
   const body = request.body;
 
-  if (!body.content) {
+  if (body.content === undefined) {
     return response.status(400).json({
       error: "content missing",
     });
   }
 
-  const note = {
-    id: generateId(),
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-  };
-  notes = notes.concat(note);
+  });
 
-  response.json(note);
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
 const unknownEndpoint = (request, response) => {
