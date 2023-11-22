1fc7a9dae3919dcdea56cd83626e179ebabc89f9
Eduardo Rivero <eduardo.rivero@unosquare.com>
2023-11-22 11:40:47 -0400

    test(api): write first integration tests

--- mongo.js
+++ mongo.js
@@ -1,13 +1,14 @@
+require('dotenv').config()
 const mongoose = require("mongoose");
 
-if (process.argv.length < 3) {
-  console.log("give password as argument");
-  process.exit(1);
-}
+// if (process.argv.length < 3) {
+//   console.log("give password as argument");
+//   process.exit(1);
+// }
 
 const password = process.argv[2];
 
-const url = `mongodb+srv://fullstack:${password}@cluster0.trei8ve.mongodb.net/noteApp?retryWrites=true&w=majority`;
+const url = process.env.TEST_MONGODB_URI
 
 mongoose.set("strictQuery", false);
 mongoose.connect(url);
@@ -24,15 +25,15 @@ const note = new Note({
   important: false,
 });
 
-// note.save().then((result) => {
-//   console.log(result);
-//   console.log("note saved!");
-//   mongoose.connection.close();
-// });
-
-Note.find({ important: false }).then((result) => {
-  result.forEach((note) => {
-    console.log(note);
-  });
+note.save().then((result) => {
+  console.log(result);
+  console.log("note saved!");
   mongoose.connection.close();
 });
+
+// Note.find({ important: false }).then((result) => {
+//   result.forEach((note) => {
+//     console.log(note);
+//   });
+//   mongoose.connection.close();
+// });
