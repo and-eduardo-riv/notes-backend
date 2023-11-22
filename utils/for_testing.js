e1f0d81685937aa79fe8474a229a890d2a4ea4de
Eduardo Rivero <eduardo.rivero@unosquare.com>
2023-11-22 10:32:17 -0400

    feat: create files to start with testing

---
+++ utils/for_testing.js
@@ -0,0 +1,18 @@
+const { argv0 } = require("process");
+
+const reverse = (string) => {
+  return string.split("").reverse().join("");
+};
+
+const average = (array) => {
+  const reducer = (sum, item) => {
+    return sum + item;
+  };
+
+  return array.length === 0 ? 0 : array.reduce(reducer, 0) / array.length;
+};
+
+module.exports = {
+  reverse,
+  average,
+};
