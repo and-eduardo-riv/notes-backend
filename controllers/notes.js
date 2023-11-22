c20d1a85434bb44684a2684e1438e089a6901431
Eduardo Rivero <eduardo.rivero@unosquare.com>
2023-11-22 17:23:11 -0400

    feat(api): populate notes and users objects in get responses

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,9 +1,9 @@
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
-const User = require('../models/user');
+const User = require("../models/user");
 
 notesRouter.get("/", async (request, response) => {
-  const notes = await Note.find({});
+  const notes = await Note.find({}).populate("user", { username: 1, name: 1 });
   response.json(notes);
 });
 
@@ -19,7 +19,7 @@ notesRouter.get("/:id", async (request, response) => {
 notesRouter.post("/", async (request, response) => {
   const body = request.body;
 
-  const user = await User.findById(body.userId)
+  const user = await User.findById(body.userId);
 
   const note = new Note({
     content: body.content,
@@ -28,8 +28,8 @@ notesRouter.post("/", async (request, response) => {
   });
 
   const savedNote = await note.save();
-  user.notes = user.notes.concat(savedNote._id)
-  await user.save()
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
 
   response.status(201).json(savedNote);
 });
