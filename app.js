92577841c1478f113b92b1549f237933c580f8b4
Eduardo Rivero <eduardo.rivero@unosquare.com>
2023-11-23 12:11:11 -0400

    feat: implement & use middlewares to extract token and user from token

--- app.js
+++ app.js
@@ -25,9 +25,12 @@ mongoose
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+app.use(middleware.tokenExtractor);
+app.use(middleware.userExtractor);
+
 app.use("/api/notes", notesRouter);
 app.use("/api/users", usersRouter);
-app.use('/api/login', loginRouter);
+app.use("/api/login", loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
